3cff0691a6642ab955e3a9988d41cfb2cb362b61
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-22 12:01:52 -0500

    Revised schema with notes and drop action

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
-### Schema
-
+-- Drop the `burgers_db` if it exists
+DROP DATABASE IF EXISTS burger_db;
+-- Create the `burgers_db`
 CREATE DATABASE burger_db;
+-- Switch to or use the `burgers_db`
 USE burger_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+-- Create a `burgers` table
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
