2772e1936db43b3994530a2ac686e94357f6f86a
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-22 13:33:04 -0500

    Setup table with mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Bacon burger');
-INSERT INTO burgers (name, sleepy) VALUES ('Bison burger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Bacon bison burger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Hamburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bison burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon bison burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger', true);
 INSERT INTO burgers (name) VALUES ('Vegan bacon cheeseburger');
