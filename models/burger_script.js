12511efb76cd91ce4003d7cd4c83e74c7d1755af
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-23 16:28:17 -0500

    Models burger script revised

--- models/burger_script.js
+++ models/burger_script.js
@@ -1,29 +1,37 @@
-// Import the ORM to create functions that will interact with the database.
+// Dependencies
+// =============================================================
 var orm = require("../config/orm.js");
 
+
+// Burger object
+// =============================================================
 var burger = {
+  //The burger object calls the orm function all and tableInput equals burgers
+  //SQL search in the orm function will search the schema for all the burgers
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.all("burgers", (res) => {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, (res) => {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, (res) => {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+    orm.delete("burgers", condition, (res) => {
       cb(res);
     });
   }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Burger Export
+// ============================================================= 
+//(burgerController.js).
 module.exports = burger;
